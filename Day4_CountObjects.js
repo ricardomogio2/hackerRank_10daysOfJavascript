7f7b2e2cf3a6cd0338d578e8259897b3b3b15bdb
RicardoMogio <47612007+ricardomogio2@users.noreply.github.com>
2019-11-05 09:38:40 -0300

    Update Day4_CountObjects.js

--- Day4_CountObjects.js
+++ Day4_CountObjects.js
@@ -2,7 +2,7 @@ function getCount(objects) {
     let count = 0
     for (let i = 0; i < objects.length; i++){
         if (objects[i].x == objects[i].y) {
-            key+=1
+            count+=1
         }
     }
     return count
